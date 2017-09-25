dd8266b3a1bd4d8c0adc1ac455d7e6b3a48cff00
SchmidtDawid <schmidtdawid@gmail.com>
2017-09-25 18:59:04 +0200

    added animated scrolling to menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
+$('.nav-item a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
 var promoElement = document.querySelectorAll(".promo-element");
 var elemRect = promoElement[0].getBoundingClientRect();
+var upButton = document.querySelector(".up-button")
 
 window.onscroll = function(){
     elemRect = promoElement[0].getBoundingClientRect();
-    if(elemRect.top - window.innerHeight + 60< 0){
+    if(elemRect.top - window.innerHeight + 60 < 0){
 
         var i = 0;
         var howManyTimes = promoElement.length;
@@ -12,11 +50,19 @@ window.onscroll = function(){
             promoElement[i].classList.add("zoom-in");
             i++;
             if(i < howManyTimes){
-                setTimeout(f, 150);
+                setTimeout(f, 170);
             }
         }
         f();
     }
+
+    if(window.pageYOffset > 500){
+        upButton.style.display = "block";
+    }
+    else{
+        upButton.style.display = "none";
+    }
+
 }
 
 
